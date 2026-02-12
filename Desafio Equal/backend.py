bad433421c960d5afb06977474785a1e0ad33921
C4rdos027 <164185022+C4rdosoo@users.noreply.github.com>
2026-02-12 20:13:49 -0300

    Criação do Backend
    Vai ser feito o front e o back, aqui esta responsável pelas merges e outras linhas de raciocínio

---
+++ Desafio Equal/backend.py
@@ -0,0 +1,63 @@
+import pandas as pd 
+import os 
+
+
+
+#lendo os arquivos 
+
+try:
+    print("importando planilhas")
+    df_vendas = pd.read_excel('fato_vendas.xlsx')
+    df_produtos = pd.read_excel('dim_produtos.xlsx')
+    df_familia = pd.read_excel('dim_familia_produtos.xlsx')
+    df_vendedor = pd.read_excel('dim_vendedor.xlsx')
+    print("importado com sucesso")
+except FileNotFoundError:
+    print("Erro: Nenhum arquivo encontrado, Verifique se estão na pasta ❌")
+    exit () 
+
+
+df_vendas ['codigo_produto'] = df_vendas ['codigo_produto'].astype(str)
+df_vendas ['codigo_vendedor'] = df_vendas['codigo_vendedor'].astype(str)
+
+df_produtos ['codigo_produto'] = df_produtos['codigo_produto'].astype(str)
+df_produtos ['codigo_familia'] = df_produtos['codigo_familia'].astype(str)
+
+df_familia['codigo_familia'] = df_familia ['codigo_familia'].astype(str)
+
+df_vendedor['codigo_vendedor'] = df_vendedor['codigo_vendedor'].astype(str)
+
+
+
+df_completo = df_vendas.merge(df_produtos, on = 'codigo_produto', how= 'left')
+df_completo = df_completo.merge(df_familia,on='codigo_familia', how='left')
+df_completo = df_completo.merge(df_vendedor,on='codigo_vendedor', how='left')
+
+
+
+df_completo['data_vendas'] = pd.to_datetime(df_completo['data_vendas'])
+df_completo['Mes_Ano'] = df_completo['data_vendas'].dt.strftime('%Y-%m')
+
+
+
+df_completo['Custo Total'] = df_completo['quantidade']*df_completo['custo_produto_unitario']
+df_completo['Receita Liquida'] = df_completo["valor_monetario_total"]
+
+
+#grantia de desconto (caso não tenha na coluna cria zerada 
+
+if'valor_desconto' in df_completo.columns:
+    df_completo['Desconto'] = df_completo['valor_desconto']
+else:
+    df_completo ['Desconto']= 0 
+
+
+#resposta 1 
+df_completo['Receita Bruta'] = df_completo['Receita Liquida'] + df_completo ['Desconto']
+
+
+#resp 5 
+df_completo['Lucro'] = df_completo['Receita Liquida'] - df_completo ['Custo Total']
+
+
+df_completo.to_excel("Lumen_Analitico_Final.xlsx", index=False)
